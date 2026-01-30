ee12af120506721076b52e169c151b8792676a45
blissexodia <binnoley@gmail.com>
2026-01-30 12:54:38 +0545

    flutter image things

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/auth.controller";
+import { authorizeUser } from "../middlewares/authorized.middleware";
 
 let authController = new AuthController();
 const router = Router();
@@ -7,7 +8,7 @@ const router = Router();
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 
-// New profile update route
-router.put("/update-profile", authController.updateProfile); // This will handle profile update
+// Protected route - requires authentication
+router.put("/update-profile", authorizeUser, authController.updateProfile);
 
 export default router;
